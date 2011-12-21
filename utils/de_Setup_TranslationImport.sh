7ff6090eeef6fd2f30d46cec45a6e5359170ff53
red1 <red1@red1.org>
2011-12-21 21:36:54 +0800

    german length limit patch - migration location

--- utils/de_Setup_TranslationImport.sh
+++ utils/de_Setup_TranslationImport.sh
@@ -7,7 +7,7 @@ GERMANHOME=/var/lib/jenkins/jobs/GermanLocalisation/workspace
 
 #APPLY MIGRATION SCRIPTS
 $WORKSPACE/adempiere/Adempiere/migration/migrate_postgresql.sh \
-$GERMANHOME/workspace/migration/postgresql commit \
+$GERMANHOME/migration/postgresql commit \
 |psql -U adempiere -d adempiere 
 
 cd $ADEMPIEREHOME/Adempiere 
